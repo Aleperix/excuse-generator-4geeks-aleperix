d1315d0747291f70a5f6ceca6b64f527fe1f2a64
Aleperix <aleperixx@gmail.com>
2022-09-26 18:32:53 +0000

    Code commented

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//Ejecutamos la función de generar excusa y agregamos el evento click al botón para generar otras.
 window.onload = function() {
   excuseGenerator();
   document.getElementById("pressme").addEventListener("click", excuseGenerator);
 };
 
+//Definimos los Arrays que generaran las excusas aleatorias.
 let who = ["the dog", "my granma", "his turtle", "my bird"];
 let what = ["eat", "pissed", "crushed", "broked"];
 let when = [
@@ -20,6 +22,9 @@ let when = [
   "while I was praying"
 ];
 
+//Función para generar las excusas, tomamos una posición de cada uno de los 3 Arrays y
+//generamos una excusa concatenándolos en un string y mostrándolos en la etiqueta con
+//el ID "excuse".
 function excuseGenerator() {
   let result = who[Math.floor(Math.random() * who.length)] + " ";
   result += what[Math.floor(Math.random() * what.length)] + " ";
