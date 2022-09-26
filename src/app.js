a15a4dd9222d322886d42acb45ab13e8cca22119
Aleperix <aleperixx@gmail.com>
2022-09-26 17:46:10 +0000

    Initial commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  excuseGenerator();
+  document.getElementById("pressme").addEventListener("click", excuseGenerator);
 };
+
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function excuseGenerator() {
+  let result = who[Math.floor(Math.random() * who.length)] + " ";
+  result += what[Math.floor(Math.random() * what.length)] + " ";
+  result += when[Math.floor(Math.random() * when.length)] + "";
+  document.getElementById("excuse").innerHTML = result;
+}
